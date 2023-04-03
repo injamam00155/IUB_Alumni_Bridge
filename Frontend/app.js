9db7c34952bbaaca923f4dfbe1ee538de4ce3f8a
r-shabab <raashidshabab@gmail.com>
2023-04-03 21:55:40 +0600

    added some background color

---
+++ Frontend/app.js
@@ -0,0 +1,5 @@
+let bookmarkBtn = document.querySelector(".bookmark-btn");
+  bookmarkBtn.addEventListener("click", function () {
+    bookmarkBtn.classList.toggle("far");
+    bookmarkBtn.classList.toggle("fas");
+  });
