fbb6238786e8481fb71b57cce38c8a3b81b70f8a
mushfiquralam <moshfiquralam@gmail.com>
2023-05-02 23:26:57 +0600

    inja bolse abar space add korte

--- IAB/database/migrations/2023_05_02_161929_create_admins_table.php
+++ IAB/database/migrations/2023_05_02_161929_create_admins_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->string('userEmail')->primary();
 
             $table->foreign('userEmail')->references('userEmail')->on('users')->onDelete('cascade');
+            
         });
     }
 
