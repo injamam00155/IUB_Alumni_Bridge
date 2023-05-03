8666793252da4658418e31a815e304105a8e3c9f
injamam00155 <injamam00155@gmail.com>
2023-05-03 18:40:48 +0600

    Post identifier added

--- IAB/app/Models/Post.php
+++ IAB/app/Models/Post.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function isQuery()
+    {
+        return DB::table('queries')->where('postID', $this->postID)->exists();
+    }
+
+    public function isJob()
+    {
+        return DB::table('jobs')->where('postID', $this->postID)->exists();
+    }
+    public function isEvent()
+    {
+        return DB::table('events')->where('postID', $this->postID)->exists();
+    }
+    public function isAward()
+    {
+        return DB::table('awards')->where('postID', $this->postID)->exists();
+    }
 }
