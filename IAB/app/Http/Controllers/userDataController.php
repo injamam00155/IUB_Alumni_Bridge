558fab09daff8742a80bcb00da3d53027cedb355
injamam00155 <injamam00155@gmail.com>
2023-05-04 04:38:28 +0600

    kisui check korte pari nai data base er karone

---
+++ IAB/app/Http/Controllers/userDataController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class userDataController extends Controller
+{
+    public function findCurrentStudentRecord($userEmail)
+    {
+        $data = DB::table('users')
+        ->join('students', 'users.userEmail', '=', 'students.userEmail')
+        ->join('current_students', 'users.userEmail', '=', 'current_students.userEmail')
+        ->where('users.userEmail', $userEmail)
+        ->select('users.*', 'students.*', 'current_students.*')
+        ->first();
+    }
+
+    
+    public function findAlumniRecord($userEmail)
+    {
+        $data = DB::table('users')
+        ->join('students', 'users.userEmail', '=', 'students.userEmail')
+        ->join('alumni', 'users.userEmail', '=', 'alumni.userEmail')
+        ->where('users.userEmail', $userEmail)
+        ->select('users.*', 'students.*', 'alumni.*')
+        ->first();
+    }
+    public function findAdmindRecord($userEmail)
+    {
+        $data = DB::table('users')
+        ->join('admins', 'users.userEmail', '=', 'admins.userEmail')
+        ->where('users.userEmail', $userEmail)
+        ->select('users.*', 'admins.*')
+        ->first();
+    }
+
+
+}
