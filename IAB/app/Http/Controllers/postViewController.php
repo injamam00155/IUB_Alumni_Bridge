c05c5df1b36532daf5af246ff2f9d9b579804485
injamam00155 <injamam00155@gmail.com>
2023-05-03 19:17:25 +0600

    slight updates

--- IAB/app/Http/Controllers/postViewController.php
+++ IAB/app/Http/Controllers/postViewController.php
@@ -2,8 +2,16 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Post;
+use App\Models\Query;
+use App\Models\Job;
+use App\Models\Event;
+use App\Models\Award;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
+
 
 class postViewController extends Controller
 {
--- IAB/app/Models/User.php
+++ IAB/app/Models/User.php
@@ -10,6 +10,7 @@ class User extends Model
 {
     use HasFactory;
     public $timestamps=false;
+    protected $fillable = ['userEmail', 'password'];
 
     public function isAdmin()
     {
